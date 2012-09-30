4fe1110e0a144cef900e8722b05c11a18ef70731
theverve <grimaldi.gerardo@gmail.com>
2012-09-30 12:08:56 -0300

    First commit.
    Extract CBZ files, and renames them to zip and vise versa, fron only one
    folder.

---
+++ ComicExt/ComicExt/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using ICSharpCode.SharpZipLib.Zip;
+using Schematrix;
+using System.IO;
+using ICSharpCode;
+
+namespace ComicExt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private Unrar unrar;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+                if (Directory.Exists(textBoxFileName.Text))
+                {
+                    //buscamos los cbz
+                    List<String> cbzFiles =
+                        new List<String>(Directory.GetFiles(textBoxFileName.Text, "*.cbz", SearchOption.AllDirectories));
+
+                    UnZip(cbzFiles);
+
+
+                    //start a new thread to unzip it
+                    //Thread th = new Thread(new ThreadStart(UnZip));
+                    //th.Start();
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+        }
+
+        private void UnZip(List<string> cbzlist)
+        {
+            foreach (var fileName in cbzlist)
+            {
+                toolStripStatusLabel1.Content = "UnZipping...";
+                var newfolder = fileName.Substring(0, fileName.Length - 4);
+                if (Directory.Exists(newfolder)) 
+                    Directory.Delete(newfolder, true); 
+                System.IO.Directory.CreateDirectory(newfolder);
+                ZipHelp.UnZip(fileName,"", newfolder);
+                List<String> picFiles = new List<String>(Directory.GetFiles(newfolder, "*.jpg", SearchOption.AllDirectories));
+                picFiles.AddRange(new List<String>(Directory.GetFiles(newfolder, "*.png", SearchOption.AllDirectories)));
+                int c = 0;
+                foreach (var picFile in picFiles)
+                {
+                    switch (Path.GetExtension(picFile))
+                    {
+                        case ".jpg":
+                            System.IO.File.Move(picFile, newfolder + @"\" + c + ".jpg");
+                            break;
+                        case ".png":
+                            System.IO.File.Move(picFile, newfolder + @"\" + c + ".png");
+                            break;
+                    }
+                    c++;
+                }
+
+                toolStripStatusLabel1.Content = "Done";
+            }
+        }
+
+        private void cbzazip_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> cbzFiles = new List<String>(Directory.GetFiles(textBoxFileName.Text, "*.cbz", SearchOption.AllDirectories));
+            foreach (var cbzFile in cbzFiles)
+            {
+                System.IO.File.Move(cbzFile, cbzFile.Substring(0, cbzFile.Length - 4) + ".zip");
+            }
+        }
+
+        private void zipacbz_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> cbzFiles = new List<String>(Directory.GetFiles(textBoxFileName.Text, "*.zip", SearchOption.AllDirectories));
+            foreach (var cbzFile in cbzFiles)
+            {
+                System.IO.File.Move(cbzFile, cbzFile.Substring(0, cbzFile.Length - 4) + ".cbz");
+            }
+        }
+        
+    }
+}
---
+++ ComicExt/ComicExt/ZipHelper.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace ComicExt
+{
+    internal class ZipHelp
+    {
+        /// <summary>
+        /// Zip a file
+        /// </summary>
+        /// <param name="SrcFile">source file path</param>
+        /// <param name="DstFile">zipped file path</param>
+        /// <param name="BufferSize">buffer to use</param>
+        public static void Zip(string SrcFile, string DstFile, int BufferSize)
+        {
+            FileStream fileStreamIn = new FileStream(SrcFile, FileMode.Open, FileAccess.Read);
+            FileStream fileStreamOut = new FileStream(DstFile, FileMode.Create, FileAccess.Write);
+            ZipOutputStream zipOutStream = new ZipOutputStream(fileStreamOut);
+
+            byte[] buffer = new byte[BufferSize];
+
+            ZipEntry entry = new ZipEntry(Path.GetFileName(SrcFile));
+            zipOutStream.PutNextEntry(entry);
+
+            int size;
+            do
+            {
+                size = fileStreamIn.Read(buffer, 0, buffer.Length);
+                zipOutStream.Write(buffer, 0, size);
+            } while (size > 0);
+
+            zipOutStream.Close();
+            fileStreamOut.Close();
+            fileStreamIn.Close();
+
+        }
+
+        /// <summary>
+        /// UnZip a file
+        /// </summary>
+        /// <param name="SrcFile">source file path</param>
+        /// <param name="DstFile">unzipped file path</param>
+        /// <param name="BufferSize">buffer to use</param>
+
+        public static void UnZip(string archiveFilenameIn, string password, string outFolder)
+        {
+            ZipFile zf = null;
+            String entryFileName = null;
+            try
+            {
+                FileStream fs = File.OpenRead(archiveFilenameIn);
+                zf = new ZipFile(fs);
+                if (!String.IsNullOrEmpty(password))
+                {
+                    zf.Password = password; // AES encrypted entries are handled automatically
+                }
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue; // Ignore directories
+                    }
+                    entryFileName = zipEntry.Name;
+                    // to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
+                    // Optionally match entrynames against a selection list here to skip as desired.
+                    // The unpacked length is available in the zipEntry.Size property.
+
+                    byte[] buffer = new byte[4096]; // 4K is optimum
+
+                    Stream zipStream = zf.GetInputStream(zipEntry);
+
+
+
+                    // Manipulate the output filename here as desired.
+                    String fullZipToPath = Path.Combine(outFolder, entryFileName);
+                    string directoryName = Path.GetDirectoryName(fullZipToPath);
+                    if (directoryName.Length > 0)
+                        Directory.CreateDirectory(directoryName);
+
+                    // Unzip file in buffered chunks. This is just as fast as unpacking to a buffer the full size
+                    // of the file, but does not waste memory.
+                    // The "using" will close the stream even if an exception occurs.
+                    using (FileStream streamWriter = File.Create(fullZipToPath))
+                    {
+                        StreamUtils.Copy(zipStream, streamWriter, buffer);
+                    }
+                }
+            }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + "The file contains an error:" + archiveFilenameIn + "\n" + "in the file " + entryFileName, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                    zf.Close(); // Ensure we release resources
+                }
+            }
+        }
+
+
+
+    }
+}
